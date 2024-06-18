fecb23c57aef9502b9bf29f90c1f43afa4bf0ba9
Brendan Gould <brendan.gould314@gmail.com>
2024-06-17 21:42:03 -0600

    Stub out cadCAD sim

---
+++ src/sim/config.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+from cadCAD.configuration import Experiment
+from cadCAD.configuration.utils import config_sim
+from cadCAD.types import (
+    Parameters,
+    PolicyOutput,
+    State,
+    StateHistory,
+    StateVariable,
+    Substep,
+)
+from networkx import DiGraph
+
+
+def compute_pub_assessment(
+    _params: Parameters, substep: Substep, state_history: StateHistory, state: State
+) -> PolicyOutput:
+    return {}
+
+
+def compute_inhereted_assessment(
+    _params: Parameters, substep: Substep, state_history: StateHistory, state: State
+) -> PolicyOutput:
+    return {}
+
+
+def simulate_purchases(
+    _params: Parameters,
+    substep: Substep,
+    state_history: StateHistory,
+    state: State,
+    _input: PolicyOutput,
+) -> Tuple[str, StateVariable]:
+    return ("", 0)
+
+
+initial_state = {"grid": DiGraph()}
+
+psubs = [
+    {
+        "policies": {
+            "pub_assessment": compute_pub_assessment,
+            "inhereted_assessment": compute_inhereted_assessment,
+        },
+        "variables": {"wallets": simulate_purchases},
+    }
+]
+
+sim_config = config_sim({"N": 2, "T": range(100), "M": {}})
+
+exp = Experiment()
+exp.append_model(
+    model_id="first_order_iw",
+    sim_configs=sim_config,
+    initial_state=initial_state,
+    partial_state_update_blocks=psubs,
+)
