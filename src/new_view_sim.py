5b11b5f1fd9e9c4f51a7ec6f798de8fd432c5e0a
Bikatr7 <Bikatr7@proton.me>
2024-08-04 18:46:37 -0600

    Converted to dash framework to allow for easier callbacks and some minor bug fixes

--- src/new_view_sim.py
+++ src/new_view_sim.py
@@ -8,11 +8,14 @@ import networkx as nx
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import plotly.io as pio
+
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
 
 from util.utils import get_latest_sim
-
-pio.renderers.default = "browser"
 
 def load_simulation_data(filepath):
     """Load the sim data from a pickle file"""
@@ -78,52 +81,37 @@ def safe_log10(x):
     """Return the log10 of x, but return -10 if x is 0"""
     return np.log10(max(abs(x), 1e-10))
 
-def create_network_trace(G, layout='grid'):
+def create_network_trace(G, layout='grid', pos=None):
     """Create plotly traces for the networkx graph"""
-
     if layout == 'grid':
         pos = {node: node for node in G.nodes()}
-
-    elif layout == 'force':
+    elif layout == 'force' and pos is None:
         pos = nx.spring_layout(G, k=1/np.sqrt(len(G.nodes())), iterations=50)
-        
-        ## Normalize positions to [0, 1]
-        x_coords = [pos[node][0] for node in G.nodes()]
-        y_coords = [pos[node][1] for node in G.nodes()]
-        x_min, x_max = min(x_coords), max(x_coords)
-        y_min, y_max = min(y_coords), max(y_coords)
-        x_range = x_max - x_min
-        y_range = y_max - y_min
-        pos = {node: ((pos[node][0] - x_min) / x_range, (pos[node][1] - y_min) / y_range) for node in G.nodes()}
 
-    node_x = [pos[node][0] for node in G.nodes()]
-    node_y = [pos[node][1] for node in G.nodes()]
+    node_x, node_y = zip(*[pos[node] for node in G.nodes()])
 
     total_wallet_values = [sum(G.nodes[node]['wallet']) for node in G.nodes()]
-    max_wallet_value = max(total_wallet_values)
-    min_wallet_value = min(total_wallet_values)
-
+    max_wallet_value, min_wallet_value = max(total_wallet_values), min(total_wallet_values)
     node_sizes = [5 + 25 * (value - min_wallet_value) / (max_wallet_value - min_wallet_value) 
                   if max_wallet_value != min_wallet_value else 15 for value in total_wallet_values]
 
     node_colors = [G.nodes[node]['color'] for node in G.nodes()]
 
-    hover_texts = []
-
-    for node in G.nodes():
-        hover_text = f"Node: {node}<br>"
-        hover_text += f"Best Vendors: {G.nodes[node]['best_vendors']}<br>"
-        hover_text += f"Wallet: {G.nodes[node]['wallet']}<br>"
-        hover_text += f"Currency Valuation: {G.nodes[node]['inherited_assessment']}<br>"
-        hover_text += f"Price: {G.nodes[node]['price']}<br>"
-        hover_text += f"Demand: {G.nodes[node]['demand']}"
-        hover_texts.append(hover_text)
+    hover_texts = [
+        f"Node: {node}<br>"
+        f"Best Vendors: {G.nodes[node]['best_vendors']}<br>"
+        f"Wallet: {G.nodes[node]['wallet']}<br>"
+        f"Currency Valuation: {G.nodes[node]['inherited_assessment']}<br>"
+        f"Price: {G.nodes[node]['price']}<br>"
+        f"Demand: {G.nodes[node]['demand']}"
+        for node in G.nodes()
+    ]
 
     edge_x, edge_y = [], []
 
     for edge in G.edges():
-        x0, y0 = pos[edge[0]][0], pos[edge[0]][1]
-        x1, y1 = pos[edge[1]][0], pos[edge[1]][1]
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
         edge_x.extend([x0, x1, None])
         edge_y.extend([y0, y1, None])
 
@@ -151,166 +139,140 @@ def create_network_trace(G, layout='grid'):
 
     return edge_trace, node_trace
 
-def create_interactive_time_evolving_network(networks, df):
-    """Create an interactive plotly figure of the time-evolving networks"""
+def calc_average_valuations(row):
+    """Calculate the average valuation of all nodes in a row"""
+    asmts = row['pricing_assessments']
+    valuation = np.array([0.0, 0.0])
+    for _, asmt in asmts.items():
+        valuation += asmt
+    valuation /= 100
+    return valuation
 
-    frames = []
+def pre_calculate_force_layouts(networks):
+    """Pre-calculate force-directed layouts for all networks"""
+    force_layouts = []
+    for G in networks:
+        pos = nx.spring_layout(G, k=1/np.sqrt(len(G.nodes())), iterations=50)
+        force_layouts.append(pos)
+    return force_layouts
+
+
+df = load_simulation_data(get_latest_sim())
+networks = create_time_evolving_network(df)
+num_timesteps = len(df) - 1
+
+# Pre-calculate force-directed layouts
+force_layouts = pre_calculate_force_layouts(networks)
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+app.layout = html.Div([
+    dcc.Graph(id='network-graph', style={'height': '80vh'}),
+    html.Div([
+        dcc.Slider(
+            id='time-slider',
+            min=0,
+            max=num_timesteps,
+            step=1,
+            value=0,
+            marks={i: {'label': str(i), 'style': {'transform': 'rotate(-45deg)'}} for i in range(0, num_timesteps + 1, 5)},
+            tooltip={"placement": "bottom", "always_visible": True}
+        ),
+    ], style={'width': '95%', 'margin': '20px auto', 'padding-top': '20px'}),
+    html.Div([
+        dbc.Button('Play/Pause', id='play-pause-button', n_clicks=0, color="primary", className="mr-2"),
+        dbc.RadioItems(
+            id='layout-toggle',
+            options=[
+                {'label': 'Grid Layout', 'value': 'grid'},
+                {'label': 'Force-Directed Layout', 'value': 'force'}
+            ],
+            value='grid',
+            inline=True,
+            style={'display': 'inline-block', 'margin-left': '20px'}
+        ),
+    ], style={'textAlign': 'center', 'margin': '20px 0'}),
+    dcc.Interval(
+        id='interval-component',
+        interval=500,  # in milliseconds
+        n_intervals=0,
+        disabled=True
+    ),
+])
+
+@app.callback(
+    Output('network-graph', 'figure'),
+    [Input('time-slider', 'value'),
+     Input('layout-toggle', 'value')]
+)
+def update_graph(time_step, layout):
+    """Update the graph based on the time step and layout selection"""
+    G = networks[time_step]
     
     fig = make_subplots(rows=2, cols=2, 
                         column_widths=[0.7, 0.3],
-                        row_heights=[0.6, 0.4],
+                        row_heights=[0.7, 0.3],
                         specs=[[{"type": "scatter", "rowspan": 2}, {"type": "scatter"}],
-                               [None, {"type": "scatter"}]])
+                               [None, {"type": "scatter"}]],
+                        vertical_spacing=0.08,
+                        horizontal_spacing=0.08)
     
-    wallet_data = {}
-
-
-    def update_layout(layout):
-        """Update the visibility of the traces based on the layout"""
-        return [
-            {'visible': [layout == 'grid', layout == 'grid', layout == 'force', layout == 'force', True] + [True] * len(wallet_data)},
-            {'title': f"{layout.capitalize()} Layout"}
-        ]
-
-    for t, G in enumerate(networks):
-        grid_edge_trace, grid_node_trace = create_network_trace(G, layout='grid')
-        force_edge_trace, force_node_trace = create_network_trace(G, layout='force')
-        
-        scatter_x = [G.nodes[node]['pricing_assessment'][0] for node in G.nodes()]
-        scatter_y = [G.nodes[node]['pricing_assessment'][1] for node in G.nodes()]
-        
-        log_x = [safe_log10(x) for x in scatter_x]
-        log_y = [safe_log10(y) for y in scatter_y]
-        
-        log_x_range = [min(log_x) - 0.1, max(log_x) + 0.1]
-        log_y_range = [min(log_y) - 0.1, max(log_y) + 0.1]
-        
-        scatter_trace = go.Scatter(
-            x=scatter_x,
-            y=scatter_y,
-            mode='markers',
-            marker=dict(
-                size=8,
-                color=grid_node_trace.marker.color,
-                colorscale='Viridis',
-                showscale=False
-            ),
-            text=[f"Node: {node}<br>Assessment: {G.nodes[node]['pricing_assessment']}" for node in G.nodes()],
-            hoverinfo='text',
-            name='Pricing Assessments'
-        )
-        
-        for node in G.nodes():
-            if node not in wallet_data:
-                wallet_data[node] = []
-            wallet_data[node].append(sum(G.nodes[node]['wallet']))
-        
-        wallet_traces = []
-        for node, values in wallet_data.items():
-            wallet_trace = go.Scatter(
-                x=list(range(t+1)),
-                y=values,
-                mode='lines',
-                name=f'Node {node}',
-                line=dict(width=1),
-                showlegend=False
-            )
-            wallet_traces.append(wallet_trace)
-        
-        avg_asmt = calc_average_valuations(df.iloc[t])
-        
-        frames.append(go.Frame(
-            data=[grid_edge_trace, grid_node_trace, force_edge_trace, force_node_trace, scatter_trace] + wallet_traces,
-            name=f't{t}',
-            layout=go.Layout(
-                title=f"Average Valuations: Red, Blue = {avg_asmt}",
-                xaxis2=dict(range=log_x_range, type="log"),
-                yaxis2=dict(range=log_y_range, type="log")
-            )
-        ))
-
-    fig.add_trace(grid_edge_trace, row=1, col=1)
-    fig.add_trace(grid_node_trace, row=1, col=1)
-    fig.add_trace(force_edge_trace, row=1, col=1)
-    fig.add_trace(force_node_trace, row=1, col=1)
+    if layout == 'force':
+        edge_trace, node_trace = create_network_trace(G, layout='force', pos=force_layouts[time_step])
+    else:
+        edge_trace, node_trace = create_network_trace(G, layout='grid')
+    
+    fig.add_trace(edge_trace, row=1, col=1)
+    fig.add_trace(node_trace, row=1, col=1)
+    
+    scatter_x = [G.nodes[node]['pricing_assessment'][0] for node in G.nodes()]
+    scatter_y = [G.nodes[node]['pricing_assessment'][1] for node in G.nodes()]
+    
+    scatter_trace = go.Scatter(
+        x=scatter_x,
+        y=scatter_y,
+        mode='markers',
+        marker=dict(
+            size=8,
+            color=node_trace.marker.color,
+            colorscale='Viridis',
+            showscale=False
+        ),
+        text=[f"Node: {node}<br>Assessment: {G.nodes[node]['pricing_assessment']}" for node in G.nodes()],
+        hoverinfo='text',
+        name='Pricing Assessments'
+    )
     fig.add_trace(scatter_trace, row=1, col=2)
     
-    for wallet_trace in wallet_traces:
-        fig.add_trace(wallet_trace, row=2, col=2)
-
-    ## Set the initial layout to grid
-    for i in range(len(fig.data)):
-        if i < 2: # Grid traces
-            fig.data[i].visible = True
-        elif i < 4:  # Force traces
-            fig.data[i].visible = False
-        else:  # Other traces (scatter, wallet)
-            fig.data[i].visible = True
-
-    updatemenus = [
-        dict(
-            type="buttons",
-            direction="left",
-            buttons=[dict(label="Play",
-                          method="animate",
-                          args=[None, {"frame": {"duration": 500, "redraw": True},
-                                       "fromcurrent": True,
-                                       "transition": {"duration": 300, "easing": "quadratic-in-out"}}]),
-                     dict(label="Pause",
-                          method="animate",
-                          args=[[None], {"frame": {"duration": 0, "redraw": False},
-                                         "mode": "immediate",
-                                         "transition": {"duration": 0}}])],
-        ),
-        dict(
-            buttons=[
-                dict(label="Grid Layout",
-                     method="update",
-                     args=update_layout('grid')),
-                dict(label="Force-Directed Layout",
-                     method="update",
-                     args=update_layout('force'))
-            ],
-            direction="down",
-            showactive=True,
-            xanchor="left",
-            y=0.9,
-            x=-0.13,
-            yanchor="top"
+    wallet_data = {}
+    for t in range(time_step + 1):
+        for node in networks[t].nodes():
+            if node not in wallet_data:
+                wallet_data[node] = []
+            wallet_data[node].append(sum(networks[t].nodes[node]['wallet']))
+    
+    for node, values in wallet_data.items():
+        wallet_trace = go.Scatter(
+            x=list(range(time_step + 1)),
+            y=values,
+            mode='lines',
+            name=f'Node {node}',
+            line=dict(width=1),
+            showlegend=False
         )
-    ]
-
+        fig.add_trace(wallet_trace, row=2, col=2)
+    
+    avg_asmt = calc_average_valuations(df.iloc[time_step])
+    
     fig.update_layout(
-        title=f"Average Valuations: Red, Blue = {calc_average_valuations(df.iloc[0])}",
+        title=f"Time Step: {time_step} | Average Valuations: Red, Blue = {avg_asmt}",
         showlegend=False,
         hovermode='closest',
-        height=1000,
-        updatemenus=updatemenus,
-        sliders=[dict(
-            active=0,
-            yanchor="top",
-            xanchor="left",
-            currentvalue=dict(
-                font=dict(size=12),
-                prefix="Time step: ",
-                visible=True,
-                xanchor="right"
-            ),
-            transition=dict(duration=300, easing="cubic-in-out"),
-            pad=dict(b=10, t=50),
-            len=0.95, 
-            x=0,
-            y=0,
-            steps=[dict(
-                method='animate',
-                args=[[f't{k}'],
-                    dict(mode='immediate',
-                        frame=dict(duration=500, redraw=True),
-                        transition=dict(duration=300, easing="quadratic-in-out"))
-                    ],
-                label=f'{k}'
-            ) for k in range(len(frames))]
-        )]
+        height=800,
+        margin=dict(l=20, r=20, t=50, b=20),
+        plot_bgcolor='rgba(227, 234, 255, 0.8)',  # Light blue background for plot areas
+        paper_bgcolor='white'  # White background for the overall figure
     )
     
     fig.update_xaxes(showgrid=False, zeroline=False, showticklabels=False, row=1, col=1)
@@ -326,7 +288,7 @@ def create_interactive_time_evolving_network(networks, df):
                      showexponent="all")
     
     fig.update_xaxes(title_text="Time Step", row=2, col=2, 
-                     range=[0, len(networks)-1],
+                     range=[0, num_timesteps],
                      dtick=10)
     
     all_wallet_values = [value for values in wallet_data.values() for value in values]
@@ -336,29 +298,30 @@ def create_interactive_time_evolving_network(networks, df):
                      exponentformat="power",
                      showexponent="all",
                      range=[safe_log10(min_wallet*0.9), safe_log10(max_wallet*1.1)])
-
-    fig.frames = frames
-
+    
     return fig
 
-def calc_average_valuations(row):
-    """Calculate the average valuation of all nodes in a row"""
-    asmts = row['pricing_assessments']
-    valuation = np.array([0.0, 0.0])
-    for _, asmt in asmts.items():
-        valuation += asmt
-    valuation /= 100
-    return valuation
+@app.callback(
+    Output('interval-component', 'disabled'),
+    [Input('play-pause-button', 'n_clicks')],
+    [State('interval-component', 'disabled')]
+)
+def toggle_interval(n_clicks, current_state):
+    """Toggle the interval component on and off"""
+    if n_clicks is None:
+        raise PreventUpdate
+    return not current_state
 
-if __name__ == "__main__":
-    df = load_simulation_data(get_latest_sim())
-    networks = create_time_evolving_network(df)
-    print(f"Created {len(networks)} networks")
-    
-    fig = create_interactive_time_evolving_network(networks, df)
-    
-    num_timesteps = len(df) - 1
-    print(f"Number of time steps in the simulation: {num_timesteps}")
-    
-    print("Displaying figure")
-    fig.show()
+@app.callback(
+    Output('time-slider', 'value'),
+    [Input('interval-component', 'n_intervals')],
+    [State('time-slider', 'value'),
+     State('time-slider', 'max')]
+)
+def update_slider(n, current_value, max_value):
+    if current_value >= max_value:
+        return 0
+    return current_value + 1
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
